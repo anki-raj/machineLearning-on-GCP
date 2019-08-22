458b07679c0de46aefd7a69d0fed2238c8ed9511
Ankit Raj <ankit.1998.ar@gmail.com>
2019-08-22 10:04:59 +0530

    Lazy evaluation

---
+++ Intro to Tensorflow/lazy_eva.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+a = tf.constant([5,3,4])
+b = tf.constant([2,3,1])
+c = tf.add(a, b)
+print("just built it, does not evaluate --> ",c)
+
+with tf.Session() as sess:
+	result = sess.run(c)
+	print("After evaluation --> ", result)
